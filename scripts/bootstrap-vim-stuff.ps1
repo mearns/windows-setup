60764f8d56170ad2bc3ed31d736761fc55e37f2d
Brian Mearns <mearns@users.noreply.github.com>
2017-05-31 15:57:57 -0400

    Update bootstrap-vim-stuff.ps1
    Remove superfluous comma that was messing up the name of the installed vimrc files.

--- scripts/bootstrap-vim-stuff.ps1
+++ scripts/bootstrap-vim-stuff.ps1
@@ -38,8 +38,8 @@ If (-Not (Test-Path "$HOME\vim_rc"))
   New-Item "$HOME\vim_rc" -Type directory
 }
 Install-Resource-File -ResourcePath "vim\vim_rc\_vimrc" -InstallPath "$HOME\vim_rc\_vimrc" -Name "Common VimRC"
-Install-Resource-File -ResourcePath "vim\.vimrc" -InstallPath "$HOME\.vimrc", -Name ".vimrc"
-Install-Resource-File -ResourcePath "vim\_vimrc" -InstallPath "$HOME\_vimrc", -Name "_vimrc"
+Install-Resource-File -ResourcePath "vim\.vimrc" -InstallPath "$HOME\.vimrc" -Name ".vimrc"
+Install-Resource-File -ResourcePath "vim\_vimrc" -InstallPath "$HOME\_vimrc" -Name "_vimrc"
 
 # Set as default editor for things that look
 Set-Env-Var -Name "EDITOR" -Value "gvim"
